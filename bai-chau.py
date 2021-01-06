a6956994e0e11da20f68926696ce364e6c83fd31
ndhcuong <ndhcuong@gmail.com>
2021-01-06 14:22:05 +0700

    bai cua chau hoan tat

---
+++ bai-chau.py
@@ -0,0 +1,14 @@
+import os
+print("tất cả các tệp và thư mục trong ổ C:")
+path = 'C:\\Users\\MyPC\\Documents'
+print(os.listdir(path))
+print("")
+
+print("các thư mục:")
+list1 = next(os.walk(path))[1]
+print(list1)
+print("")
+
+print("các tệp:")
+list2 = next(os.walk(path))[2]
+print(list2)
